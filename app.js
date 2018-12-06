3fa1e781def4097e463ef9d6bfc57ec5d4eec28f
Ray <chen2733@purdue.edu>
2018-12-06 16:30:36 -0500

    Admin Login/Session/Cookie Parser

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var cluster = require('cluster');
+/* var cluster = require('cluster');
 
 if (cluster.isMaster) {
     var cpuCount = require('os').cpus().length;
@@ -11,64 +11,115 @@ if (cluster.isMaster) {
         console.log('Worker ' + worker.id + ' died :(');
         cluster.fork();
     });
-} else {
-    var AWS = require('aws-sdk'),
-        bodyParser = require('body-parser'),
-        express = require('express'),
-        app = express(),
-        port = process.env.PORT || 3000;
+} else {} */
 
-    AWS.config.region = process.env.REGION;
+var AWS = require('aws-sdk'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    express = require('express'),
+    session = require('express-session'),
+    app = express(),
+    port = process.env.PORT || 3000;
 
-    AWS.config.apiVersions = {
-        dynamodb: '2012-08-10',
-    };
+AWS.config.region = process.env.REGION;
 
-    var ddb_doc = new AWS.DynamoDB.DocumentClient();
-    var table = "OrderTable";
-    var food = ['Hamburger', 'Fries', 'Wings', 'Cookie', 'Salads', 'Orange Juice'];
+AWS.config.apiVersions = {
+    dynamodb: '2012-08-10',
+};
 
-    app.use(express.static('static'));
-    app.use(bodyParser.urlencoded({extended: false}));
-    app.use(bodyParser.json());
+var ddb_doc = new AWS.DynamoDB.DocumentClient();
+var table = "OrderTable";
+var food = ['Hamburger', 'Fries', 'Wings', 'Cookie', 'Salads', 'Orange Juice'];
 
-    app.get('/', function (req, res) {
-        res.sendFile(__dirname + '/static/index.html');
-    });
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+    secret: "Online-Deli",
+    resave: false,
+    saveUninitialized: false,
+}));
 
-    app.post('/make_order', function (req, res) {
-        console.log('POST SUCCESS');
+app.use(function (req, res, next) {
+        console.log('Authorizing ' + req.url);
 
-        var order = {
-            "orderID": req.body["orderID"],
-            "Address": req.body["Address"],
-            "Total Before Tax": req.body["Total Before Tax"],
-            "Tax": req.body["Tax"],
-            "Total": req.body["Total"],
-            "Feedback": req.body["Feedback"]
-        };
-
-        for (var i = 0; i < food.length; i++) {
-            if (req.body.hasOwnProperty(food[i])) {
-                order[food[i]] = req.body[food[i]];
+        if (req.url === '/admin.html' || req.url === '/SearchFeedback.html'
+            || req.url === 'SearchInventory.html') {
+            if (req.session && req.session.authenticated) {
+                res.sendFile(__dirname + '/private' + req.url);
+            } else {
+                res.sendStatus(404);
             }
+
+            return;
         }
 
-        var params = {
-            TableName: table,
-            Item: order
-        };
+        next();
+    }
+);
 
-        ddb_doc.put(params, function (err, data) {
-            if (err) {
-                console.error("Unable to add item");
-            } else {
-                console.log("Added item");
-            }
-        });
-    });
+app.get('/', function (req, res) {
+    res.redirect('/index.html');
+});
 
-    app.listen(port, function () {
-        console.log('Server running at http://127.0.0.1:' + port + '/');
+app.get('/logout', function (req, res, next) {
+    delete req.session.authenticated;
+    res.redirect('/index.html');
+});
+
+app.post('/make_order', function (req, res) {
+    console.log('POST SUCCESS');
+
+    var order = {
+        "orderID": req.body["orderID"],
+        "Address": req.body["Address"],
+        "Total Before Tax": req.body["Total Before Tax"],
+        "Tax": req.body["Tax"],
+        "Total": req.body["Total"],
+        "Feedback": req.body["Feedback"]
+    };
+
+    for (var i = 0; i < food.length; i++) {
+        if (req.body.hasOwnProperty(food[i])) {
+            order[food[i]] = req.body[food[i]];
+        }
+    }
+
+    var params = {
+        TableName: table,
+        Item: order
+    };
+
+    ddb_doc.put(params, function (err, data) {
+        if (err) {
+            console.error("Unable to add item");
+        } else {
+            console.log("Added item");
+        }
     });
-}
+});
+
+app.post('/validate', function (req, res) {
+    console.log('POST SUCCESS');
+    console.log(req.body);
+
+    if (req.body.email && req.body.email === "abc@gmail.com" &&
+        req.body.password && req.body.password === "123") {
+        req.session.authenticated = true;
+        res.redirect('/admin.html');
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+app.use(function (req, res, next) {
+    console.log('General request SUCCESS');
+    console.log(req.url);
+    console.log(req.body);
+    next();
+});
+
+app.listen(port, function () {
+    console.log('Server running at http://127.0.0.1:' + port + '/');
+});
